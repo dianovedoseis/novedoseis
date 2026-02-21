21bd30d1f388257bb4af3d21c75cc4adddc822d8
dianovedoseis <afrent.contato@gmail.com>
2026-02-21 14:21:35 -0300

    Add files via upload
    updated index

---
+++ script.js
@@ -0,0 +1,30 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const countDownDate = new Date("2026-06-09T00:00:00").getTime();
+
+const updateCountdown = () => {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    daysEl.innerText = days;
+    hoursEl.innerText = hours;
+    minutesEl.innerText = minutes;
+    secondsEl.innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById("countdown").innerHTML = "<h2>O abismo foi aberto!</h2>";
+    }
+};
+
+const interval = setInterval(updateCountdown, 1000);
+
+updateCountdown();
